e686124361af49414970fa2f2ee657973c5ad871
Craigory Coppola <craigorycoppola@gmail.com>
2024-08-12 12:45:36 -0400

    fix(markdown-factory): add jsdoc / tsdoc comments

--- packages/markdown-factory/src/lib/markdown.ts
+++ packages/markdown-factory/src/lib/markdown.ts
@@ -1,69 +1,280 @@
+/**
+ * @license Apache-2.0
+ * @module markdown-factory
+ *
+ * @description A simple library to generate markdown strings.
+ */
+
+/**
+ * Function to create an arbitrary heading
+ * @param level The level of the heading. Must be between 1 and 6.
+ * @param title The title of the heading.
+ * @param contents The contents of the section.
+ * @returns The markdown heading, with the contents section below it.
+ *
+ * @example
+ * ```typescript
+ * console.log(h(1, 'Heading 1', 'This is the contents of the section.'));
+ * // Prints:
+ * //
+ * // # Heading 1
+ * //
+ * // This is the contents of the section.
+ * ```
+ */
 export function h(level: number, title: string, ...contents: string[]) {
   assert(level >= 1, 'Heading level must be >= 1.');
   assert(level <= 6, 'Most markdown engines only support heading levels 1-6.');
   return lines(`${'#'.repeat(level)} ${title}`, contents);
 }
 
+/**
+ * Function to create a level 1 heading.
+ * @param title The title of the heading.
+ * @param contents The contents of the section.
+ * @returns The markdown heading, with the contents section below it.
+ *
+ * @example
+ * ```typescript
+ * console.log(h1('Heading 1', 'This is the contents of the section.'));
+ * // Prints:
+ * //
+ * // # Heading 1
+ * //
+ * // This is the contents of the section.
+ * ```
+ */
 export function h1(title: string, ...contents: string[]) {
   return h(1, title, ...contents);
 }
 
+/**
+ * Function to create a level 2 heading.
+ * @param title The title of the heading.
+ * @param contents The contents of the section.
+ * @returns The markdown heading, with the contents section below it.
+ *
+ * @example
+ * ```typescript
+ * console.log(h2('Heading 2', 'This is the contents of the section.'));
+ * // Prints:
+ * //
+ * // ## Heading 2
+ * //
+ * // This is the contents of the section.
+ * ```
+ */
 export function h2(title: string, ...contents: string[]) {
   return h(2, title, ...contents);
 }
 
+/**
+ * Function to create a level 3 heading.
+ * @param title The title of the heading.
+ * @param contents The contents of the section.
+ * @returns The markdown heading, with the contents section below it.
+ *
+ * @example
+ * ```typescript
+ * console.log(h3('Heading 3', 'This is the contents of the section.'));
+ * // Prints:
+ * //
+ * // ### Heading 3
+ * //
+ * // This is the contents of the section.
+ * ```
+ */
 export function h3(title: string, ...contents: string[]) {
   return h(3, title, ...contents);
 }
 
+/**
+ * Function to create a level 4 heading.
+ * @param title The title of the heading.
+ * @param contents The contents of the section.
+ * @returns The markdown heading, with the contents section below it.
+ *
+ * @example
+ * ```typescript
+ * console.log(h4('Heading 4', 'This is the contents of the section.'));
+ * // Prints:
+ * //
+ * // #### Heading 4
+ * //
+ * // This is the contents of the section.
+ * ```
+ */
 export function h4(title: string, ...contents: string[]) {
   return h(4, title, ...contents);
 }
 
+/**
+ * Function to create a level 5 heading.
+ * @param title The title of the heading.
+ * @param contents The contents of the section.
+ * @returns The markdown heading, with the contents section below it.
+ *
+ * @example
+ * ```typescript
+ * console.log(h5('Heading 5', 'This is the contents of the section.'));
+ * // Prints:
+ * //
+ * // ##### Heading 5
+ * //
+ * // This is the contents of the section.
+ * ```
+ */
 export function h5(title: string, ...contents: string[]) {
   return h(5, title, ...contents);
 }
 
+/**
+ * Function to create a level 6 heading.
+ * @param title The title of the heading.
+ * @param contents The contents of the section.
+ * @returns The markdown heading, with the contents section below it.
+ *
+ * @example
+ * ```typescript
+ * console.log(h6('Heading 6', 'This is the contents of the section.'));
+ * // Prints:
+ * //
+ * // ###### Heading 6
+ * //
+ * // This is the contents of the section.
+ * ```
+ */
 export function h6(title: string, ...contents: string[]) {
   return h(6, title, ...contents);
 }
 
+/**
+ * Function to link to an external resource.
+ * @param ref The reference to link to.
+ * @param title The title/label of the link.
+ * @returns Markdown link to the external resource.
+ * @example
+ * ```typescript
+ * console.log(link('https://example.com', 'Example'));
+ * // Prints:
+ * //
+ * // [Example](https://example.com)
+ * ```
+ */
 export function link(ref: string, title?: string) {
   return `[${title ?? ref}](${ref})`;
 }
 
+/**
+ * Function to create italicized text.
+ * @param contents The text to italicize.
+ * @returns Markdown italicized text.
+ */
 export function italics(contents: string) {
   return `*${contents}*`;
 }
 
+/**
+ * Function to create bold text.
+ * @param contents The text to bold.
+ * @returns Markdown bold text.
+ */
 export function bold(contents: string) {
   return `**${contents}**`;
 }
 
+/**
+ * Function to create strikethrough text.
+ * @param contents The text to strikethrough.
+ * @returns Markdown strikethrough text.
+ */
 export function strikethrough(contents: string) {
   return `~~${contents}~~`;
 }
 
+/**
+ * Function to create inline code.
+ * @param contents The code to inline.
+ * @returns Markdown inline code.
+ */
 export function code(contents: string) {
   return `\`${contents}\``;
 }
 
+/**
+ * Function to create a code block.
+ * @param contents The code to include in the block.
+ * @param language The language the code is written in.
+ * @returns Markdown code block.
+ */
 export function codeBlock(contents: string, language?: string) {
   return `\`\`\`${language ? language : ''}
 ${contents}
 \`\`\``;
 }
 
-type SimpleTableField<T> = keyof T;
-type MappedTableField<T> = {
+/**
+ * A simple table field. A string, that's a key of the object supplied to the {@link table} function.
+ * @typeParam T The type of the objects in the array in the table.
+ * @example
+ * ```typescript
+ * const items = [{name: 'Alice', age: 30}, {name: 'Bob', age: 40}];
+ * console.log(table(items, ['name', 'age']));
+ * // Prints:
+ * //
+ * // | name  | age |
+ * // | ----  | --- |
+ * // | Alice | 30  |
+ * ```
+ */
+export type SimpleTableField<T> = keyof T;
+
+/**
+ * A mapped table field. A field with a label and a mapping function.
+ *
+ * @typeParam T The type of the objects in the array in the table.
+ * @example
+ * ```typescript
+ * const items = [{name: 'Alice', age: 30}, {name: 'Bob', age: 40}];
+ * console.log(table(items, [{label: 'Name', mapFn: (el) => el.name}, {label: 'Dog Age', mapFn: (el) => el.age * 7}]));
+ * // Prints:
+ * //
+ * // | Name  | Age  |
+ * // | ----  | ---  |
+ * // | Alice | 210  |
+ * // | Bob   | 280  |
+ * ```
+ */
+export type MappedTableField<T> = {
   label: string;
-  mapFn: (el: T) => string;
+  mapFn: (el: T) => string | { toString(): string };
 };
-type RenamedTableField<T> = {
+
+/**
+ * A renamed table field. A field with a label and a key of the object supplied to the {@link table} function.
+ * @typeParam T The type of the objects in the array in the table.
+ *
+ * @example
+ * ```typescript
+ * const items = [{name: 'Alice', age: 30}, {name: 'Bob', age: 40}];
+ * console.log(table(items, [{label: 'Name', field: 'name'}, {label: 'Age', field: 'age'}]));
+ * // Prints:
+ * //
+ * // | Name  | Age |
+ * // | ----  | --- |
+ * // | Alice | 30  |
+ * // | Bob   | 40  |
+ * ```
+ */
+export type RenamedTableField<T> = {
   label: string;
   field: keyof T;
 };
 
+/**
+ * Type to represent a field in a table. Can be a {@link SimpleTableField}, a {@link MappedTableField}, or a {@link RenamedTableField}.
+ * @typeParam T The type of the objects in the array in the table.
+ */
 export type TableField<T extends Record<string, unknown>> =
   | SimpleTableField<T>
   | MappedTableField<T>
@@ -71,17 +282,37 @@ export type TableField<T extends Record<string, unknown>> =
 
 function normalizeTableField<T extends Record<string, unknown>>(
   f: TableField<T>
-): MappedTableField<T> {
+): Omit<MappedTableField<T>, 'mapFn'> & {
+  mapFn: (...params: Parameters<MappedTableField<T>['mapFn']>) => string;
+} {
   return {
     label: typeof f === 'object' ? f.label : f.toString(),
     mapFn:
       typeof f === 'object' && 'mapFn' in f
-        ? f.mapFn
+        ? (e) => f.mapFn(e).toString()
         : // eslint-disable-next-line @typescript-eslint/no-explicit-any
           (e) => (e[typeof f === 'object' ? f.field : f] as any).toString(),
   };
 }
 
+/**
+ * Function to create a table from an array of objects.
+ * @param items The array of objects to create the table from.
+ * @param fields The fields to include in the table. See {@link TableField}.
+ * @typeParam T The type of the objects in the array. Should be inferred from the `items` parameter.
+ * @returns Markdown table.
+ * @example
+ * ```typescript
+ * const items = [{name: 'Alice', age: 30}, {name: 'Bob', age: 40}];
+ * console.log(table(items, ['name', 'age']));
+ * // Prints:
+ * //
+ * // | name  | age |
+ * // | ----  | --- |
+ * // | Alice | 30  |
+ * // | Bob   | 40  |
+ * ```
+ */
 export function table<
   T extends Record<string, unknown> = Record<string, unknown>
 >(items: T[], fields: TableField<T>[]): string {
@@ -111,8 +342,13 @@ export function table<
   ].join('\n');
 }
 
-export function blockQuote(...fragments: string[]): string {
-  const mappedFragments = fragments.map((f) =>
+/**
+ * Function to create a blockquote.
+ * @param lines Lines to be blockquoted.
+ * @returns The blockquoted markdown.
+ */
+export function blockQuote(...lines: string[]): string {
+  const mappedFragments = lines.map((f) =>
     f
       .split('\n')
       .map((line) => `> ${line}`)
@@ -121,27 +357,142 @@ export function blockQuote(...fragments: string[]): string {
   return mappedFragments.join('\n>\n').trim();
 }
 
+/**
+ * Options for {@link orderedList} / {@link ol}.
+ */
 export type OrderedListOptions = { level?: number; startIdx?: number };
+
+/**
+ * Options for {@link unorderedList} / {@link ul}.
+ */
 export type UnorderedListOptions = { level?: number };
 
-// Sig 1
+/**
+ * Creates an unordered list.
+ *
+ * @param items Each item in the list.
+ * @returns The unordered list markdown.
+ * @example
+ * ```typescript
+ * console.log(unorderedList('item1', 'item2', 'item3'));
+ * // Prints:
+ * //
+ * // - item1
+ * //
+ * // - item2
+ * //
+ * // - item3
+ * ```
+ **/
 export function unorderedList(...items: string[]): string;
 
-// Sig 2
+/**
+ * Creates an unordered list.
+ *
+ * @param items Each item in the list.
+ * @returns The unordered list markdown.
+ * @example
+ * ```typescript
+ * console.log(unorderedList('item1', 'item2', 'item3'));
+ * // Prints:
+ * //
+ * // - item1
+ * //
+ * // - item2
+ * //
+ * // - item3
+ * ```
+ **/
 export function unorderedList(items: string[]): string;
 
-// Sig 3
+/**
+ * Creates an unordered list.
+ *
+ * @param options The options for the unordered list. See {@link UnorderedListOptions}.
+ * @param items Each item in the list.
+ * @returns The unordered list markdown.
+ * @example
+ * ```typescript
+ * console.log(
+ *   unorderedList(
+ *     'item1',
+ *     lines(
+ *       'item 2',
+ *       unorderedList({level: 2}, 'item a', 'item b')
+ *     ),
+ *     'item3'
+ *   )
+ * );
+ * // Prints:
+ * //
+ * // - item1
+ * //
+ * // - item2
+ * //
+ * //   - item a
+ * //
+ * //   - item b
+ * //
+ * // - item3
+ * ```
+ **/
 export function unorderedList(
   options: UnorderedListOptions,
   ...items: string[]
 ): string;
 
-// Sig 4
+/**
+ * Creates an unordered list.
+ *
+ * @param options The options for the unordered list. See {@link UnorderedListOptions}.
+ * @param items Each item in the list.
+ * @returns The unordered list markdown.
+ * @example
+ * ```typescript
+ * console.log(
+ *   unorderedList(
+ *     'item1',
+ *     lines(
+ *       'item 2',
+ *       unorderedList({level: 2}, 'item a', 'item b')
+ *     ),
+ *     'item3'
+ *   )
+ * );
+ * // Prints:
+ * //
+ * // - item1
+ * //
+ * // - item2
+ * //
+ * //   - item a
+ * //
+ * //   - item b
+ * //
+ * // - item3
+ * ```
+ **/
 export function unorderedList(
   options: UnorderedListOptions,
   items: string[]
 ): string;
 
+/**
+ * Creates an unordered list.
+ *
+ * @returns The unordered list markdown.
+ * @example
+ * ```typescript
+ * console.log(unorderedList('item1', 'item2', 'item3'));
+ * // Prints:
+ * //
+ * // - item1
+ * //
+ * // - item2
+ * //
+ * // - item3
+ * ```
+ **/
 export function unorderedList(
   ...itemOrOptions:
     | [string?, ...string[]]
@@ -170,14 +521,117 @@ export function unorderedList(
   const level = (options?.level ?? 1) - 1;
   return lines(items.map((i) => `${'\t'.repeat(level)}- ${i}`));
 }
+
+/**
+ * See {@link unorderedList}.
+ **/
 export const ul = unorderedList;
 
+/**
+ * Creates an ordered list.
+ *
+ * @param items Each item in the list.
+ * @returns The ordered list markdown.
+ * @example
+ * ```typescript
+ * console.log(orderedList('item1', 'item2', 'item3'));
+ * // Prints:
+ * //
+ * // 1. item1
+ * //
+ * // 2. item2
+ * //
+ * // 3. item3
+ * ```
+ **/
 export function orderedList(...items: string[]): string;
+
+/**
+ * Creates an ordered list.
+ *
+ * @param items Each item in the list.
+ * @returns The ordered list markdown.
+ * @example
+ * ```typescript
+ * console.log(orderedList('item1', 'item2', 'item3'));
+ * // Prints:
+ * //
+ * // 1. item1
+ * //
+ * // 2. item2
+ * //
+ * // 3. item3
+ * ```
+ **/
 export function orderedList(items: string[]): string;
+
+/**
+ * Creates an ordered list.
+ *
+ * @param options The options for the ordered list. See {@link OrderedListOptions}.
+ * @param items Each item in the list.
+ * @returns The ordered list markdown.
+ * @example
+ * ```typescript
+ * console.log(
+ *   orderedList(
+ *     'item1',
+ *     lines(
+ *       'item 2',
+ *       orderedList({level: 2}, 'item a', 'item b')
+ *     ),
+ *     'item3'
+ *   )
+ * );
+ * // Prints:
+ * //
+ * // 1. item1
+ * //
+ * // 2. item2
+ * //
+ * //   1. item a
+ * //
+ * //   2. item b
+ * //
+ * // 3. item3
+ * ```
+ **/
 export function orderedList(
   options: OrderedListOptions,
   ...items: string[]
 ): string;
+
+/**
+ * Creates an ordered list.
+ *
+ * @param options The options for the ordered list. See {@link OrderedListOptions}.
+ * @param items Each item in the list.
+ * @returns The ordered list markdown.
+ * @example
+ * ```typescript
+ * console.log(
+ *   orderedList(
+ *     'item1',
+ *     lines(
+ *       'item 2',
+ *       orderedList({level: 2}, 'item a', 'item b')
+ *     ),
+ *     'item3'
+ *   )
+ * );
+ * // Prints:
+ * //
+ * // 1. item1
+ * //
+ * // 2. item2
+ * //
+ * //   1. item a
+ * //
+ * //   2. item b
+ * //
+ * // 3. item3
+ * ```
+ **/
 export function orderedList(
   options: OrderedListOptions,
   items: string[]
@@ -212,12 +666,27 @@ export function orderedList(
     items.map((i, idx) => `${'\t'.repeat(level)}${startIdx + idx}. ${i}`)
   );
 }
+
+/**
+ * See {@link orderedList}.
+ **/
 export const ol = orderedList;
 
+/**
+ * Utility function to create new lines within markdown. Joins all strings with two new lines, as single new lines are ignored in markdown.
+ * @param ls Each line in the markdown.
+ * @returns Combined markdown.
+ */
 export function lines(...ls: (string[] | string)[]) {
   return ls.flat().join('\n\n');
 }
 
+/**
+ * Function to create a link to a header within the same markdown file.
+ * @param header Which header to link to.
+ * @param linkText Optional text to display for the link.
+ * @returns The markdown link.
+ */
 export function linkToHeader(header: string, linkText?: string): string {
   const sanitized = header
     .toLowerCase()
@@ -226,6 +695,12 @@ export function linkToHeader(header: string, linkText?: string): string {
   return link(`#${sanitized}`, linkText ?? header);
 }
 
+/**
+ * Function to create a table of contents from the headers in the markdown.
+ * @param maxDepth The maximum depth of the table of contents.
+ * @param s The markdown to generate the table of contents from.
+ * @returns The table of contents markdown.
+ */
 export function tableOfContents(maxDepth: number, ...s: string[]) {
   const allLines = s.flatMap((l) => l.split('\n'));
   type Section = { depth: number; label: string; children?: Section[] };
@@ -326,6 +801,11 @@ export function tableOfContents(maxDepth: number, ...s: string[]) {
   );
 }
 
+/**
+ * Function to create formatted front matter for markdown files.
+ * @param metadata The metadata to include in the front matter.
+ * @param format The format to output the front matter in. Defaults to 'yaml'.
+ */
 export function frontMatter(
   metadata: Record<string, unknown>,
   format: 'json' | 'yaml' = 'yaml'
@@ -336,6 +816,7 @@ export function frontMatter(
     case 'yaml':
       return yamlFrontMatter(metadata);
   }
+  throw new Error(`Unsupported front matter format: ${format}`);
 }
 
 function jsonFrontMatter(metadata: Record<string, unknown>): string {
